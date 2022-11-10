f362f0f900cc0d2ef21832e84f54b9734c07eab5
sbg96 <93029334+SHINBOGEUN@users.noreply.github.com>
2022-11-10 15:38:45 +0900

    lombok추가 로깅추가

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,11 +1,18 @@
 package com.example.firstproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@AllArgsConstructor // 생성자
+@ToString
+@NoArgsConstructor
 public class Article {
 
     @Id //대푯값을 지정! = 주민등록번호 같은
@@ -18,18 +25,5 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
 
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
