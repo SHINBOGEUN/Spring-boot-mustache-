c476467183f016d23184fc33f04f2bddbe336ce6
sbg96 <93029334+SHINBOGEUN@users.noreply.github.com>
2022-11-10 16:54:25 +0900

    테이블 조회

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 @Entity
 @AllArgsConstructor // 생성자
 @ToString
-@NoArgsConstructor
+@NoArgsConstructor //디폴트 생성자를 추가!
 public class Article {
 
     @Id //대푯값을 지정! = 주민등록번호 같은
--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -4,6 +4,11 @@ import com.example.firstproject.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article,Long> {
 
+    //findAll() ArrayList로 변경
+    @Override
+    ArrayList<Article> findAll();
 }
