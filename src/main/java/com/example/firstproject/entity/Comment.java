c82b070d801a05d670a73346c0b2fc32bd75fd65
sbg96 <93029334+SHINBOGEUN@users.noreply.github.com>
2022-11-15 18:37:25 +0900

    Comment RestAPI Test

--- src/main/java/com/example/firstproject/entity/Comment.java
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.example.firstproject.entity;
 
+import com.example.firstproject.dto.CommentDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,4 +31,21 @@ public class Comment {
     private String body;
 
 
+    public static Comment createComment(CommentDto dto, Article article) {
+        //예외 발생
+        if(dto.getId() != null){
+            throw new IllegalArgumentException("댓글 생성 실패!");
+        }
+        if(dto.getArticleId() != article.getId()){
+            throw new IllegalArgumentException("댓글 생성 실패! id가 잘못되었습니다.");
+        }
+        //엔티티 생성 및 반환
+        return new Comment(
+                dto.getId(),
+                article,
+                dto.getNickname(),
+                dto.getBody()
+        );
+
+    }
 }
